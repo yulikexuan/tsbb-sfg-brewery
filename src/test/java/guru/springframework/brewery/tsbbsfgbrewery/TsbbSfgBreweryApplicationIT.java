a36586c57a3961deaae80aadccfc1f64bf48f59d
yul <yu.li@tecsys.com>
2019-03-21 10:46:00 -0400

    Initialize Spring-Boot with JUnit 5

---
+++ src/test/java/guru/springframework/brewery/tsbbsfgbrewery/TsbbSfgBreweryApplicationIT.java
@@ -0,0 +1,16 @@
+package guru.springframework.brewery.tsbbsfgbrewery;
+
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class TsbbSfgBreweryApplicationIT {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
